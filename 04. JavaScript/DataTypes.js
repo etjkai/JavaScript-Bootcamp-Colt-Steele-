ab6985a40e51ee60e07e936571cc65861c5304e4
Eugene Teo <etjkai@gmail.com>
2020-06-07 10:10:03 +0800

    JavaScript portions

---
+++ 04. JavaScript/DataTypes.js
@@ -0,0 +1,28 @@
+// 1) Numbers
+// 2) Strings
+// 3) Booleans
+// 4) Null
+// 5) Undefined
+
+// Length property of string
+'HELLO'.length
+'HELLO'[3]
+'The Beatles'.length
+
+// Assigning a variable
+var numInitial = 43
+
+// Changing its value
+numInitial = 64
+
+// Change value + Type
+numInitial = "64"
+
+// undefined - declared but not initialized to a value
+// var age;
+
+ // null - explicitly nothing
+
+
+// Prompt
+// var username = prompt("What is your name?")
