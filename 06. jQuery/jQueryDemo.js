6d52e5b79c96e444d34ac0a3271d18f252e923d3
Eugene Teo <etjkai@gmail.com>
2020-06-18 22:17:29 +0800

    jQuery things

---
+++ 06. jQuery/jQueryDemo.js
@@ -0,0 +1,13 @@
+// Selecting with jQuery - returns a list  
+
+// select all IMG tags
+$("img")
+
+// select sale class
+$(".sale")
+
+// select bonus class
+$("#bonus")
+
+//select a tags inside li
+$("li a")
